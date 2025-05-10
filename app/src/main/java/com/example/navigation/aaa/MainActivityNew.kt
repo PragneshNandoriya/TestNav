816ef7551c6dffe69d8b36588d02904a829b9386
Pragnesh2014 <pragneshec2014@gmail.com>
2025-05-10 23:40:57 +0530

    added comment

--- app/src/main/java/com/example/navigation/aaa/MainActivityNew.kt
+++ app/src/main/java/com/example/navigation/aaa/MainActivityNew.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import com.example.navigation.R
 import com.example.navigation.aaa.PaymentActivity
@@ -20,5 +21,6 @@ class MainActivityNew : Activity() {
         findViewById<Button>(R.id.btn_kotlin).setOnClickListener {
             startActivity(Intent(this, PaymentActivity::class.java))
         }
+        Log.i("MainActivity", "onCreate: ")
     }
 }
